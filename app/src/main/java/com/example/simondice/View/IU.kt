d9d88d3d3fa887aced8557beb57a945c59e9e6d5
sromerodiaz-dc <sromerodiaz@danielcastelao.org>
2024-11-27 18:17:35 +0100

    Primer boton creado

--- app/src/main/java/com/example/simondice/View/IU.kt
+++ app/src/main/java/com/example/simondice/View/IU.kt
@@ -1,7 +1,14 @@
 package com.example.simondice.View
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -10,64 +17,74 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-/*
-@Composable
-fun unaTostada() {
-
-}
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}*/
-
+import com.example.simondice.Model.Datos
+import com.example.simondice.ViewModel.MyViewModel
 
 @Composable
-fun MisBotones(){
+fun StartButton(myViewModel: MyViewModel) {
+    // Variable observer
+    var estado by remember { mutableStateOf(myViewModel.estadoLiveData.value!!.boton_activo) }
 
-    // Variables observadas...
+    // Set context
+    myViewModel.estadoLiveData.observe(LocalLifecycleOwner.current) {
+        estado = myViewModel.estadoLiveData.value!!.start_activo
+    }
 
+    // Variable observer
+    var ronda by remember { mutableStateOf(myViewModel.counterLiveData.value!!) }
 
-    Column {
-        // mostrar el contador de clics
+    // Set context
+    myViewModel.counterLiveData.observe(LocalLifecycleOwner.current) {
+        ronda = myViewModel.counterLiveData.value!!
+    }
+
+    // Composición principal
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // Título
+        Text(
+            text = "SIMON DICE",
+            fontSize = 30.sp,
+            modifier = Modifier.padding(vertical = 100.dp)
+        )
+
+        // Botón de inicio
         TextButton(
+            enabled = estado,
             onClick = {
-                Log.d("botones", "click!")
-                /*counter++
-                pulsaciones.add(Colores.ROJO.valor)
-
-                colores_pulsados.add(Colores.ROJO.nombre)
-
-                Log.d("botones", colores_pulsados.toString())*/
-            }) {
-            //Text("CLICS: para el log y $counter")
-        }
-/*
-        // mientras no tecleamos mas de tres caracteres no se muestra el saludo
-        if (name.value.length > 3) {
-            Text(
-                text = "Nombre: ${name.value}!",
-                fontSize = 24.sp
-            )
-        }
-        // campo de texto para rellenar
-        TextField(
-            value = name.value,
-            onValueChange = {
-                name.value = it
+                // Lógica del botón aun por hacer
             },
-            label = { Text(text = "Name") }
-        )*/
+            modifier = Modifier
+                .padding(10.dp)
+                .size(100.dp, 100.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Gray,
+                contentColor = Color.White
+            )
+        ) {
+            Text(text = "INICIAR\n(ronda número: $ronda!)")
+        }
+
+        // Aquí puedes agregar otros componentes o estructuras
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Column {
+                // Columna 1: Botones u otros elementos
+            }
+            Column {
+                // Columna 2: Botones u otros elementos
+            }
+        }
     }
 }
-/*
-@Preview(showBackground = true)
-@Composable
-fun myPreview() {
-
-}*/
