a752d5b7e3dbaf53265c71e7e574f37b650b01a4
sromerodiaz-dc <sromerodiaz@danielcastelao.org>
2024-11-27 17:43:26 +0100

    Nueva variable observer

--- app/src/main/java/com/example/simondice/ViewModel/MyViewModel.kt
+++ app/src/main/java/com/example/simondice/ViewModel/MyViewModel.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.MutableState
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.simondice.Model.Corutinas
 import com.example.simondice.Model.Datos
 
 class MyViewModel(): ViewModel() {
@@ -14,6 +15,7 @@ class MyViewModel(): ViewModel() {
     // Variables observer
     val nombreLiveData: MutableLiveData<MutableState<String>> = MutableLiveData(Datos.nombre)
     val counterLiveData: MutableLiveData<MutableIntState> = MutableLiveData(Datos.counter)
+    val estadoLiveData: MutableLiveData<Corutinas> = MutableLiveData(Corutinas.INICIO)
 
     // Constructor
     init {
