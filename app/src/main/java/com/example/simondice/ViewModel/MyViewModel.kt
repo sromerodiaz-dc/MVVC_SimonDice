7a08b0c349875609ca5092fd1e365606d28f14d2
sromerodiaz-dc <sromerodiaz@danielcastelao.org>
2024-11-27 18:21:01 +0100

    Observables creados

--- app/src/main/java/com/example/simondice/ViewModel/MyViewModel.kt
+++ app/src/main/java/com/example/simondice/ViewModel/MyViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.simondice.ViewModel
 
 import android.util.Log
+import androidx.collection.MutableIntList
 import androidx.compose.runtime.MutableIntState
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.simondice.Model.Corutinas
@@ -12,10 +14,13 @@ class MyViewModel(): ViewModel() {
     // TAG
     private val TAG_LOG = "viewModel-DEBUG"
 
-    // Variables observer
-    val nombreLiveData: MutableLiveData<MutableState<String>> = MutableLiveData(Datos.nombre)
+    // Variables observables
+    val textLiveData: MutableLiveData<MutableState<String>> = MutableLiveData(Datos.text)
     val counterLiveData: MutableLiveData<MutableIntState> = MutableLiveData(Datos.counter)
     val estadoLiveData: MutableLiveData<Corutinas> = MutableLiveData(Corutinas.INICIO)
+    val secuenciaLiveData: MutableLiveData<MutableList<Int>> = MutableLiveData(Datos.secuencia)
+    val playerGuessLiveData: MutableLiveData<MutableList<Int>> = MutableLiveData(Datos.playerGuess)
+    val historicoLiveData: MutableLiveData<MutableState<Int>> = MutableLiveData(Datos.historico)
 
     // Constructor
     init {
