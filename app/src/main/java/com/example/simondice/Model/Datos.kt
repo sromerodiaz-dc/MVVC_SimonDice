d956eeadcad11520c6b72e84046addcde8fab217
sromerodiaz-dc <sromerodiaz@danielcastelao.org>
2024-11-27 18:18:04 +0100

    Nuevos datos necesarios para el juego añadidos

--- app/src/main/java/com/example/simondice/Model/Datos.kt
+++ app/src/main/java/com/example/simondice/Model/Datos.kt
@@ -1,20 +1,15 @@
 package com.example.simondice.Model
 
-import android.graphics.Color
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 
 // Datos que serán observados por el controller (MyViewModel)
 object Datos {
-    var nombre = mutableStateOf("")
-    var counter = mutableIntStateOf(0)
-}
-
-enum class Colores(val color: Int, val txt: String) {
-    ROJO(color = Color.RED, txt = "rojo"),
-    VERDE(color = Color.GREEN, txt = "verde"),
-    AZUL(color = Color.BLUE, txt = "azul"),
-    AMARILLO(color = Color.YELLOW, txt = "amarillo")
+    val text = mutableStateOf("")
+    val counter = mutableIntStateOf(0)
+    val secuencia = mutableListOf<Int>()
+    val playerGuess = mutableListOf<Int>()
+    val historico = mutableIntStateOf(0)
 }
 
 // Corutinas de ejemplo
