f038265dcae40da82e1972dfbb537a3f84297c59
sromerodiaz-dc <sromerodiaz@danielcastelao.org>
2024-11-27 11:42:36 +0100

    Clase Datos comentada e implementando corutinas de ejemplo

--- app/src/main/java/com/example/simondice/Model/Datos.kt
+++ app/src/main/java/com/example/simondice/Model/Datos.kt
@@ -16,3 +16,10 @@ enum class Colores(val color: Int, val txt: String) {
     AZUL(color = Color.BLUE, txt = "azul"),
     AMARILLO(color = Color.YELLOW, txt = "amarillo")
 }
+
+// Corutinas de ejemplo
+enum class Corutinas(val start_activo: Boolean, val boton_activo: Boolean) {
+    INICIO(start_activo = true, boton_activo = false),
+    GENERANDO(start_activo = false, boton_activo = false),
+    ADIVINANDO(start_activo = false, boton_activo = true)
+}
