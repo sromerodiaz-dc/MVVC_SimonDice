2bb98d784c70e3258f2f8c4f13d0c099eee9739e
sromerodiaz-dc <sromerodiaz@danielcastelao.org>
2024-11-27 18:39:42 +0100

    Boton normal añadido y codigo comentado

--- app/src/main/java/com/example/simondice/View/IU.kt
+++ app/src/main/java/com/example/simondice/View/IU.kt
@@ -1,5 +1,7 @@
 package com.example.simondice.View
 
+import android.content.Context
+import android.provider.CalendarContract.Colors
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -8,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -21,9 +24,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.semantics.Role.Companion.Button
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.simondice.Model.Colores
 import com.example.simondice.Model.Datos
 import com.example.simondice.ViewModel.MyViewModel
 
@@ -86,5 +91,38 @@ fun StartButton(myViewModel: MyViewModel) {
                 // Columna 2: Botones u otros elementos
             }
         }
+    }
+}
+
+@Composable
+fun BotoNormal(
+    color: Colores, // Objeto que contiene información del botón (nombre, id, color, etc.)
+    context: Context, // Contexto para operaciones con ViewModel
+    myViewModel: MyViewModel, // ViewModel para la lógica de negocio
+    colorDefault: Color = color.color // Objeto que contiene el color empleado
+) {
+    // Variable observer
+    var estado by remember { mutableStateOf(myViewModel.estadoLiveData.value!!.boton_activo) }
+
+    // Set context
+    myViewModel.estadoLiveData.observe(LocalLifecycleOwner.current) {
+        estado = myViewModel.estadoLiveData.value!!.boton_activo
+    }
+
+    // Definición del botón
+    Button(
+        enabled = estado, // Estado del botón (activo/inactivo)
+        onClick = {
+            // Lógica del botón aún por implementar
+        },
+        modifier = Modifier
+            .padding(10.dp) // Margen externo del botón
+            .size(150.dp, 100.dp), // Dimensiones del botón
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Gray, // Color de fondo del botón
+            contentColor = Color.White // Color del texto del botón
+        )
+    ) {
+        Text(text = "..."+color.nombre)
     }
 }
